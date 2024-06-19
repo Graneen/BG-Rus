095cb966d26732f7213887c73c04f1672e7950e4
IvanRazumov <149903009+IvanRazumov@users.noreply.github.com>
2024-06-20 00:08:45 +0300

    Types

--- client/src/pages/gameMeet/GameMeet.tsx
+++ client/src/pages/gameMeet/GameMeet.tsx
@@ -1,41 +1,41 @@
-// import React, { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { RootState } from '../../redux/store';
-// import { joinGameSession, createGameSession } from '../../redux/slice'; 
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { joinGameSession, createGameSession, GameSessionState } from '../../redux/slice'; 
 
-// const GameMeet: React.FC = () => {
-//   const [sessionId, setSessionId] = useState('');
-//   const [playerName, setPlayerName] = useState('');
-//   const [sessionName, setSessionName] = useState('');
-//   const [maxPlayers, setMaxPlayers] = useState(0);
+const GameMeet: React.FC = () => {
+  const [sessionId, setSessionId] = useState('');
+  const [playerName, setPlayerName] = useState('');
+  const [sessionName, setSessionName] = useState('');
+  const [maxPlayers, setMaxPlayers] = useState(0);
 
-//   const gameSession = useSelector((state: RootState) => state.gameSession);
-//   const dispatch = useDispatch();
+  const gameSession = useSelector((state: RootState) => state.gameSession as GameSessionState);
+  const dispatch = useDispatch();
 
-//   const handleJoinSession = () => {
-//     dispatch(joinGameSession({ sessionId, playerName })); 
-//   };
+  const handleJoinSession = () => {
+    dispatch(joinGameSession({ sessionId, playerName })); 
+  };
 
-//   const handleCreateSession = () => {
-//     dispatch(createGameSession({ sessionName, maxPlayers })); 
-//   };
+  const handleCreateSession = () => {
+    dispatch(createGameSession({ sessionName, maxPlayers })); 
+  };
 
-//   return (
-//     <div>
-//       <h2>Join Game Session</h2>
-//       <form onSubmit={handleJoinSession}>
-//         <input type="text" value={sessionId} onChange={(e) => setSessionId(e.target.value)} />
-//         <input type="text" value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
-//         <button type="submit">Join</button>
-//       </form>
-//       <h2>Create Game Session</h2>
-//       <form onSubmit={handleCreateSession}>
-//         <input type="text" value={sessionName} onChange={(e) => setSessionName(e.target.value)} />
-//         <input type="number" value={maxPlayers} onChange={(e) => setMaxPlayers(parseInt(e.target.value))} />
-//         <button type="submit">Create</button>
-//       </form>
-//     </div>
-//   );
-// };
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h2>Записаться на игровую сессию</h2>
+      <form onSubmit={handleJoinSession}>
+        <input type="text" value={sessionId} onChange={(e) => setSessionId(e.target.value)} />
+        <input type="text" value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
+        <button type="submit">Записаться</button>
+      </form>
+      <h2>Создать игровую сессию</h2>
+      <form onSubmit={handleCreateSession}>
+        <input type="text" value={sessionName} onChange={(e) => setSessionName(e.target.value)} />
+        <input type="number" value={maxPlayers} onChange={(e) => setMaxPlayers(parseInt(e.target.value))} />
+        <button type="submit">Создать</button>
+      </form>
+    </div>
+  );
+};
 
-// export default GameMeet;
+export default GameMeet;
