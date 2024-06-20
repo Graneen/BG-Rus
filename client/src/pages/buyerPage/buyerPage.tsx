653c4511e1c7b451b07ce42c41aad149f59ab897
IvanRazumov <149903009+IvanRazumov@users.noreply.github.com>
2024-06-21 00:14:01 +0300

    Last

--- client/src/pages/buyerPage/buyerPage.tsx
+++ client/src/pages/buyerPage/buyerPage.tsx
@@ -1,10 +1,10 @@
 function BuyerPage() {
     return ( 
-       
+        <>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             Buyers page
         </div>
-        
+        </>
      );
 }
 
