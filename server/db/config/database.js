ffeadb0bb84d7ebce2f8519c9b817f134bf0379a
KireevSS <114350303+KireevSS@users.noreply.github.com>
2024-06-21 23:50:21 +0300

    Update database.js

--- server/db/config/database.js
+++ server/db/config/database.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: "postgres",
-    password: "postgres",
-    database: "bgrus_db_2",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
