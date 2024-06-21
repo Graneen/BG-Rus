81bd828451347cfeb5bf06bc54594a3f7b4ef891
KireevSS <sergo89@hotmail.com>
2024-06-22 01:39:43 +0300

    ///////////

--- server/db/config/database.js
+++ server/db/config/database.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
   },
 };
