a5668199d10e4c474f231f412dd3292921508b9a
BansheeTDD <55banshee55@gmail.com>
2024-06-27 23:30:55 +0600

    check 2

--- client/src/services/axiosConfig/axiosConfig.ts
+++ client/src/services/axiosConfig/axiosConfig.ts
@@ -9,8 +9,6 @@ const $api = axios.create({
     // baseURL: API_URL,
 });
 
-
-
 $api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
     return config;
